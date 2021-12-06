2476d4c59a14d563336e7241e547afd55489c16b
Macir Junior <mj.brustolin@hotmail.com>
2021-12-06 09:56:55 -0300

    Aulas 3 e 4

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log("Macir 'Brustolin'");
+console.log('Macir "Brustolin"');
+console.log(`'Macir' "Brustolin"`);
+
+console.log(35, 15.85, 'Macir Brustolin');
+
+console.log('Meu nome é "Macir". Estou aprendendo JavaScript às', 10, "da manhã");
