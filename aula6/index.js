418d8486c01bcb144a71da380bfce0168b218e0e
Macir Junior <mj.brustolin@hotmail.com>
2021-12-06 10:35:27 -0300

    Aula 6 - Creating and Initializing Variables

---
+++ aula6/index.js
@@ -0,0 +1,22 @@
+let nome = 'João';   //Declaração moderna, utilizar assim
+var nome2 = 'Maria'; //Declaração mais antiga
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O filho de", nome, "e Maria se chama Eduardo.");
+
+
+let nome3; //Declarando a variável
+nome3 = 'qualquer valor'; // Inicializando a variável
+console.log(nome3);
+nome3 = 'macir';
+console.log(nome3);
+
+//Não podemos criar variáveis com palavras reservadas (IF, LET, Etc.).
+//Variáveis precisam ter nomes significativos.
+//Não pode começar o nome da variável com um número.
+//Não podem conter espaços ou traços.
+//è utilizado o camelCase.
+//Case sensitive (letras maiúsculas e minúsculas fazem diferença).
